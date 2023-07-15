6a94972df653bc439464614071c2a8295631c84b
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-07-15 10:50:11 +0300

    add theme resources

--- app/src/main/java/com/shaban/pizza/ui/theme/Font.kt
+++ app/src/main/java/com/shaban/pizza/ui/theme/Font.kt
@@ -1,2 +1,12 @@
 package com.shaban.pizza.ui.theme
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.shaban.pizza.R
+
+val Poppins = FontFamily(
+    Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.poppins_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.poppins_semi_bold, weight = FontWeight.SemiBold),
+)
--- app/src/main/java/com/shaban/pizza/ui/theme/Type.kt
+++ app/src/main/java/com/shaban/pizza/ui/theme/Type.kt
@@ -2,33 +2,71 @@ package com.shaban.pizza.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    // region Title
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        color = Black
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        color = Black
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        color = Black
+    ),
+    // endregion
+
+    // region Body
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        color = Black
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = Black
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        color = Black
+    ),
+    // endregion
+
+    // region Label
+    labelLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        color = Black
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        color = Black
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        color = Black
     )
-    */
+    // endregion
 )
